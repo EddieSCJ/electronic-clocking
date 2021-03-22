922d97380495428c58197f21656b72e94c3e0834
EddieSCJ <eddiestudyifs@gmail.com>
2021-03-21 21:26:12 -0300

    Starting project

---
+++ src/main/kotlin/com/edcleidson/eletronicclocking/EletronicClockingApplication.kt
@@ -0,0 +1,11 @@
+package com.edcleidson.eletronicclocking
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class EletronicClockingApplication
+
+fun main(args: Array<String>) {
+	runApplication<EletronicClockingApplication>(*args)
+}
---
+++ src/test/kotlin/com/edcleidson/eletronicclocking/EletronicClockingApplicationTests.kt
@@ -0,0 +1,13 @@
+package com.edcleidson.eletronicclocking
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class EletronicClockingApplicationTests {
+
+	@Test
+	fun contextLoads() {
+	}
+
+}
